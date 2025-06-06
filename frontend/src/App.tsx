c772e888269b9dbe660b5d51c943eba467885ade
Naji Kadri <najikadri2000@gmail.com>
2025-06-06 05:41:05 +0400

    Setup frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className="flex flex-col text-center">
+      <h1>Welcome to the React App</h1>
+      <p>This is placeholder for the initial setup.</p>
+    </div>
+  )
+}
+
+export default App
